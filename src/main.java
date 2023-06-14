12a10cfec818c3cac6a36bc11eb7694f5704b5d6
daniel mondragon <daniel.mondragon004@GMAIL.COM>
2023-06-14 09:07:26 -0500

    Syntax, Types, and Variables

--- src/main.java
+++ src/main.java
@@ -1,5 +1,48 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello,World!");
+
+        String myString = "Hello,World!";
+        System.out.println(myString);
+
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+        float myNumber = 314;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int y = 5;
+//        System.out.println(++y);
+//        System.out.println(y);
+
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(theNumberThree);
+//        System.out.println(o);
+//        System.out.println(three);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
